8f3e9d91d417026a6aff6d6b39e83e6ff4e0dbdf
Alexander Lee Rivera <arivera.lee@temple.edu>
2014-11-10 21:13:37 -0500

    First commit

---
+++ DisFunctions.h
@@ -0,0 +1,9 @@
+#ifndef DIS_FUNC
+#define DIS_FUNC
+#include <stdio.h>
+#include <stdlib.h>
+
+void assembly(int[], int[], char **, int**, int);
+void printAssem(char **, char **, char **, int**, int);
+
+#endif 
